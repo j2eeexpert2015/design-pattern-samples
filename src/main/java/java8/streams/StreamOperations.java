5c5a601869da6f2febd9ade66781d33a3ad9bb22
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2021-10-28 16:25:11 +0530

    Added New Samples

--- src/main/java/java8/streams/StreamOperations.java
+++ src/main/java/java8/streams/StreamOperations.java
@@ -3,8 +3,10 @@ package java8.streams;
 import java8.common.Employee;
 
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamOperations {
@@ -13,19 +15,49 @@ public class StreamOperations {
         System.out.println(employeeList);
         Map<String, Long> noOfMaleAndFemaleEmployees=
                 employeeList.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
-
         System.out.println("noOfMaleAndFemaleEmployees:"+noOfMaleAndFemaleEmployees);
+
+        Map<String, List<Employee>> genderVsListOfEmployees=
+                employeeList.stream().collect(Collectors.groupingBy(p->p.getGender()));
+        //System.out.println("genderVsListOfEmployees:"+genderVsListOfEmployees);
+
+        Map<String, Map<String, List<Employee>>> genderVsDeptVsListOfEmployees=
+                employeeList.stream().collect(Collectors.groupingBy(p->p.getGender(),Collectors.groupingBy(p->p.getDepartment())));
+        //System.out.println("genderVsDeptVsListOfEmployees:"+genderVsDeptVsListOfEmployees);
+
+        Map<String, DoubleSummaryStatistics> genderVsSumOfSalary =
+        employeeList.stream().collect(Collectors.groupingBy(p->p.getGender(),Collectors.summarizingDouble(p->p.getSalary())));
+        System.out.println("genderVsSumOfSalary:"+genderVsSumOfSalary);
+        /*
+        Map<String, Employee> groupingByGenderAndEmployeeList=
+                employeeList.stream().collect(Collectors.toMap(Employee::getGender, Function.identity()));
+        System.out.println("groupingByGenderAndEmployeeList:"+groupingByGenderAndEmployeeList);
+        */
+
+        /*
+        employeeList.stream()
+                .collect(Collectors
+                        .groupingBy(
+                                e -> e,
+                                Collectors.summarizingInt(e -> e.getSalary())
+                        )
+                )
+                .forEach((k,v) -> {
+                    k.scope = v.getSum();
+                    System.out.println(k);
+                });
+    */
     }
 
     public static List<Employee> generateEmployeeData()
     {
         List<Employee> employeeList = new ArrayList<>();
 
-        employeeList.add(new Employee(1, "Emp1", 32, "Female", "HR", 2011, 25000.0));
-        employeeList.add(new Employee(2, "Emp2", 25, "Male", "Sales And Marketing", 2015, 13500.0));
-        employeeList.add(new Employee(3, "Emp3", 29, "Male", "Infrastructure", 2012, 18000.0));
-        employeeList.add(new Employee(4, "Emp4", 28, "Male", "Product Development", 2014, 32500.0));
-        employeeList.add(new Employee(5, "Emp6", 27, "Female", "HR", 2013, 22700.0));
+        employeeList.add(new Employee(1, "Emp1", 20, "Female", "HR", 2011, 100));
+        employeeList.add(new Employee(2, "Emp2", 30, "Male", "HR", 2015, 200));
+        employeeList.add(new Employee(3, "Emp3", 40, "Male", "SALES", 2012, 100));
+        employeeList.add(new Employee(4, "Emp4", 35, "Male", "SALES", 2014, 500));
+        employeeList.add(new Employee(5, "Emp6", 25, "Female", "HR", 2013, 300));
         return employeeList;
     }
 }
